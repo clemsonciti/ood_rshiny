1d9739f4368bec33adf1d81ed1ecd9f1a7723f10
Linh B Ngo <lngo@wcupa.edu>
2022-01-15 21:10:35 -0500

    Update after.sh

--- template/after.sh
+++ template/after.sh
@@ -1,13 +1,16 @@
-# Wait for the Jupyter Notebook server to start
-echo "Waiting for Jupyter Notebook server to open port ${port}..."
-echo "TIMING - Starting wait at: $(date)"
-if wait_until_port_used "${host}:${port}" 60; then
-  echo "Discovered Jupyter Notebook server listening on port ${port}!"
-  echo "TIMING - Wait ended at: $(date)"
-else
-  echo "Timed out waiting for Jupyter Notebook server to open port ${port}!"
-  echo "TIMING - Wait ended at: $(date)"
-  pkill -P ${SCRIPT_PID}
-  clean_up 1
-fi
-sleep 2
+# Wait for the NGINX server to start
+TIME=120
+FOUND=0
+echo "Waiting for NGINX server to open port ${port}..."
+for ((i=1; i<=TIME*2; i++)); do
+  if ! kill -0 ${SCRIPT_PID} 2>/dev/null; then
+    echo "Script used to launch NGINX server exited early!"
+    clean_up 1
+  elif port_used "${port}"; then
+    FOUND=1
+    echo "Discovered NGINX server listening on port ${port}!"
+    break
+  fi
+  sleep 0.5
+done
+[[ ${FOUND} -eq 0 ]] && echo "Timed out waiting for NGINX server!" && clean_up 1
